d9fd71317c9c32f246efd341914ec7952818c64e
TorryTheMaltese <torryiscute@gmail.com>
2021-01-14 15:34:39 +0900

    create temporary views

---
+++ src/routes/routes.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+import MainView from "../views/MainView";
+import LoginView from "../views/LoginView";
+import JoinView from "../views/JoinView";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "main",
+      component: MainView,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginView,
+    },
+    {
+      path: "/join",
+      name: "join",
+      component: JoinView,
+    },
+  ],
+});
---
+++ src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="margin:0;">
+    <header-nav></header-nav>
+  </div>
+</template>
+
+<script>
+import HeaderNav from "../components/HeaderNav";
+export default {
+  components: {
+    HeaderNav,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
