2b790d130b58254cbcf3f2820bef5e4e4c8558d6
TorryTheMaltese <torryiscute@gmail.com>
2021-02-18 09:47:15 +0900

    Edit docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,7 @@ module.exports = {
   ],
   output: {
     filename: "[name].js",
-    path: path.join(__dirname, "dist"),
-    publicPath: "/dist",
+    path: path.join(__dirname, "docs"),
+    publicPath: "/docs",
   },
 };
